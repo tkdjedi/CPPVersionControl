16343c328ef4d745742fa72f71da77380563a78f
Tristan <tkdjedi@gmail.com>
2024-07-25 10:50:33 -0700

    main's new lyrics

--- CPPVersionControl/main.cpp
+++ CPPVersionControl/main.cpp
@@ -9,6 +9,7 @@ void greet()
 int main()
 {
     // print some text
-    std::cout << "Raise this barn!!\n\n";
+    std::cout << "Raise this barn!!\n";
+    std::cout << "Together we can racist barn!!\n\n";
     return 0;
 }
